17f6ec558a278dae2448ca4920efc96c845cd3ee
urizennnnnn <igamerryt@gmail.com>
2023-09-28 22:23:53 +0100

    Errors fixed

--- server.js
+++ server.js
@@ -6,22 +6,14 @@ const app = express();
 const routes = require('./routes/routes');
 const fileupload = require('express-fileupload');
 const morgan = require('morgan')
-// const cloudinary = require('cloudinary').v2.config({
-//     cloud_name: process.env.CLOUD_NAME,
-//     api_key: process.env.CLOUD_API_KEY,
-//     api_secret: process.env.CLOUD_API_SECRET
-// })
-
-// database
 app.use(express.json());
 app.use(morgan('dev'))
-// Assuming this is where you serve videos
 app.use(express.static('./server'))
 
 
 // middleware
 app.use(fileupload());
-// Define routes
+//  routes
 app.use('/api/HNG', routes);
 
 // Error handling middleware
